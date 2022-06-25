d914b7140b99c7eea75a856b719cce65276cb1bc
Shambu1329 <shambukrishna34@gmail.com>
2022-06-25 23:57:12 +0530

    completed

---
+++ employe.js
@@ -0,0 +1,45 @@
+loginbtnevent=()=>{
+    employename=(document.getElementById("employename").value)
+    password=(document.getElementById("password").value)
+    console.log(employename)
+    alert(employename)
+
+    console.log(password)
+    alert(password)
+}
+regbtnevent=()=>{
+    employename=(document.getElementById("employeename").value)
+    address=(document.getElementById("address").value)
+    age=(document.getElementById("age").value)
+    emailid=(document.getElementById("emailid").value)
+    mobileno=(document.getElementById("mobileno").value)
+    yearofexperience=(document.getElementById("yearofexperience").value)
+    password=(document.getElementById("password").value)
+    confirmpassword=(document.getElementById("confirmpassword").value)
+
+    console.log(employename)
+    alert(employename)
+
+    console.log(address)
+    alert(address)
+
+    console.log(age)
+    alert(age)
+
+    console.log(emailid)
+    alert(emailid)
+
+    console.log(mobileno)
+    alert(mobileno)
+
+    console.log(yearofexperience)
+    alert(yearofexperience)
+
+    console.log(password)
+    alert(password)
+
+    console.log(confirmpassword)
+    alert(confirmpassword)
+
+    
+}
